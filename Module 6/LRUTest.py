b9a538d9b3c32aa7d9b2dc88fb6b1ecd83b325ab
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-04-13 23:15:45 +0530

    final

--- Module 6/LRUTest.py
+++ Module 6/LRUTest.py
@@ -2,11 +2,21 @@ from LRUCache import LRUCache
 
 def main():
     test = LRUCache(3)
-    test.put(1, "Sai")
-    test.put(2, "Teja")
-    test.put(3, "Kondapalli")
-    assert test.cache_dict == {1: "Sai", 2: "Teja", 3: "Kondapalli"}
+    test.put(1, "Hyderabad")
+    test.put(2, "Delhi")
+    test.put(3, "Mumbai")
+    assert test.cache_dict == {1: "Hyderabad", 2: "Delhi", 3: "Mumbai"}
     print("Put test case passed")
+    assert test.get(2) == "Delhi"
+    print("Get test case passed")
+    assert test.cache_dict == {1: "Hyderabad", 3: "Mumbai", 2: "Delhi"}
+    print("After get dictionary is correct")
+    test.put(4,"Bangalore")
+    assert test.cache_dict == {3: "Mumbai", 2: "Delhi", 4: "Bangalore"}
+    print("put after cache exceed passed")
+    test.put(3, "Hyderabad")
+    assert test.cache_dict == {3: "Hyderabad", 2: "Delhi", 4: "Bangalore"}
+    print("LRU Success")
 
 if __name__ == "__main__":
     main()
