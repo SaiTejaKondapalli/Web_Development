6a6fcc4551536642543ef8be062d9b46f0de7bb7
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-04-13 22:06:15 +0530

    Testcases

--- Module 6/LRUCache.py
+++ Module 6/LRUCache.py
@@ -1,11 +1,10 @@
 class LRUCache:
     def __init__(self, capacity):
-        super().__init__()
         self.capacity = capacity
+        self.cache_dict = OrderedDict()
 
     def put(self,key,value):
-        pass
+
     def get(self, key):
-        pass
+
     def get_cache(self):
-        pass
---
+++ Module 6/LRUTest.py
@@ -0,0 +1,12 @@
+from LRUCache import LRUCache
+
+def main():
+    test = LRUCache(3)
+    test.put(1, "Sai")
+    test.put(2, "Teja")
+    test.put(3, "Kondapalli")
+    assert test.input_dict = {1: "Sai", 2: "Teja", 3: "Kondapalli"}
+    print("Put test case passed")
+
+if __name__ == "__main__":
+    main()
