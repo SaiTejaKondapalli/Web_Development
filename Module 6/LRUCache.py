61aeff3af68d929448162b12c8da70d631d3d1ee
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-04-13 19:55:28 +0530

    adding method signatures

--- Module 6/LRUCache.py
+++ Module 6/LRUCache.py
@@ -2,3 +2,10 @@ class LRUCache:
     def __init__(self, capacity):
         super().__init__()
         self.capacity = capacity
+
+    def put(self,key,value):
+        pass
+    def get(self, key):
+        pass
+    def get_cache(self):
+        pass
