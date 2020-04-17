c8d7b8e421b7ec2c372d89d8a033a9cdb5ee1cf6
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-04-17 21:14:13 +0530

    table created

---
+++ Project 1/Database.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import Column, String, DateTime,create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+db_base = declarative_base()
+
+class User(db_base):
+    __tablename__ = "users"
+    email = Column(String(30), primary_key=True)
+    name = Column(String(30), nullable=False)
+    pswd = Column(String(50), nullable=False)
+    timestamp = Column(DateTime(timezone=True), nullable=False)
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db_base.metadata.create_all(engine)
