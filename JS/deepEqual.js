a7243dfae4af95a1d09c0e2e7ae32fbdfadf641d
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-04-28 17:53:39 +0530

    deepEqual

--- JS/deepEqual.js
+++ JS/deepEqual.js
@@ -1,32 +1,67 @@
 function deepEqual(a, b) {
-    if (a === b)
-        return true;
-    if (a == null || typeof a != 'object' || b == null || typeof b != 'object')
-        return false;
-    var propsInA = 0, propsInB = 0;
-    for (var prop in a)
-         propsInA += 1;
-    for (var prop in b) {
-        propsInB += 1;
-        if (!(prop in a) || !deepEqual(a[prop], b[prop]))
-            return false;
-    }
-    return propsInA == propsInB;
+
+	if (typeof a === 'object' && typeof b === 'object') {
+
+		let count = 0
+		const aKeys = Object.keys(a)
+		const bKeys = Object.keys(b)
+
+		for (let i = 0; i < aKeys.length; i++) {
+
+			for (let j = 0; j < bKeys.length; j++) {
+
+				if (deepEqual(a[aKeys[i]], b[bKeys[j]])) {
+					count++
+					break
+				}
+
+			}
+		}
+		if (count === aKeys.length && count === bKeys.length)
+			return true
+		else
+			return false
+	}
+	else
+		return a === b
 }
 
-a = {
-    name:"teja",
-    marks:90,
-    age: 23,
-    address: {city : "hyderabad"}
-}
-b=
-{
-    name:"teja",
-    marks:90,
-    age: 23,
-    address: {city : "hyderabad"}
+const one = {
+	1:'hello',
+	2:'world',
 }
 
-ans = deepEqual(a,b)
-console.log(ans)
+const two = {
+	1:'hello',
+	2:'world',
+}
+
+const three = {
+	1:'hello',
+	2:'world',
+	inner: {
+		key:"value"
+	},
+}
+
+const four = {
+	1:'hello',
+	2:'world',
+	inner: {
+		key:"value"
+	},
+}
+
+const obj = {
+	here: {
+		is: "an"
+	},
+	object: 2
+}
+
+console.log(deepEqual(5, 5))
+console.log(deepEqual(one, one))
+console.log(deepEqual(one, three))
+console.log(deepEqual(three, four))
+console.log(deepEqual(obj, obj))
+console.log(deepEqual(obj,{object: 2, here: {is: "an"}}))
